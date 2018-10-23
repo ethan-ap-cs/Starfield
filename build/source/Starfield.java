71e9ad54b158e0568fb8e088b7a378e5a54504d7
ethanostrow <ethan.ostrow7@gmail.com>
2018-10-23 00:16:54 -0700

    this is annoying

--- build/source/Starfield.java
+++ build/source/Starfield.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 
 Particle [] p;
-void setup(){
-	size(400,400);
+public void setup(){
+	
 	p = new Particle[100];
 	for (int i = 0; i < p.length; i++){
 		p[i] = new NormalParticle();
@@ -10,7 +26,7 @@ void setup(){
 	p[2] = new OddballParticle(90);
 	p[3] = new OddballParticle(310);
 }
-void draw(){
+public void draw(){
 	for (int i = 0; i < p.length; i++){
 		p[i].move();
 		p[i].show();
@@ -60,4 +76,14 @@ class OddballParticle implements Particle{
 }
 class JumboParticle extends NormalParticle{
 
+}
+  public void settings() { 	size(400,400); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
